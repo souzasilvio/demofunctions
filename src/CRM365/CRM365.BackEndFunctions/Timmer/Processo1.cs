cc04caffd5fdfba15e7bb426af9f88eff37585b4
Silvio Jose De Souza <sjoses@indracompany.com>
2021-12-07 15:45:35 -0300

    Ajuste timer

--- src/CRM365/CRM365.BackEndFunctions/Timmer/Processo1.cs
+++ src/CRM365/CRM365.BackEndFunctions/Timmer/Processo1.cs
@@ -7,7 +7,7 @@ namespace CRM365.BackEndFunctions.Timmer
     public static class Processo1
     {
         [FunctionName("Processo1")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static void Run([TimerTrigger("%TimerProcesso1%")]TimerInfo myTimer, TraceWriter log)
         {
             string msg = $"C# Timer trigger function executed at: {DateTime.Now}";
             log.Info(msg);
